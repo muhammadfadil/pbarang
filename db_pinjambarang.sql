43182a5441b8f400102f984ffdfc70f17bb31c62
andrefjr <andrefjr11@gmail.com>
2018-10-29 18:47:33 +0700

    tambah tabel
    add tabel barang

--- db_pinjambarang.sql
+++ db_pinjambarang.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2018 at 02:39 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: 29 Okt 2018 pada 12.45
+-- Versi Server: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_login`
+-- Struktur dari tabel `tbl_login`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_login` (
@@ -33,11 +33,37 @@ CREATE TABLE IF NOT EXISTS `tbl_login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_login`
+-- Dumping data untuk tabel `tbl_login`
 --
 
 INSERT INTO `tbl_login` (`id`, `username`, `password`) VALUES
 (1, 'admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_barang` (
+`id_barang` int(11) NOT NULL,
+  `kode_barang` varchar(50) DEFAULT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `merk/type` varchar(50) NOT NULL,
+  `no_seri` varchar(50) DEFAULT NULL,
+  `kondisi_barang` varchar(50) NOT NULL,
+  `unit` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data untuk tabel `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `kode_barang`, `nama_barang`, `merk/type`, `no_seri`, `kondisi_barang`, `unit`) VALUES
+(1, '30303010437-', 'spektrum analyzer', 'anritsu ms 2720t', '1536028', 'baik', 1),
+(2, '3060324005', 'antena dipole', 'anritsu mp534b', '', 'baik', 1),
+(3, NULL, 'gps garmin', 'montana 680', '30303010726', 'baik', 1),
+(4, NULL, 'kabel 10 meter', '', NULL, 'baik', 1);
 
 --
 -- Indexes for dumped tables
@@ -49,6 +75,21 @@ INSERT INTO `tbl_login` (`id`, `username`, `password`) VALUES
 ALTER TABLE `tbl_login`
  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+ ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
