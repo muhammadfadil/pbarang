e6afec4a0dc06cba0ca007b94abdc91785ba59e7
isalanand <faisalanand@gmail.com>
2018-10-30 14:54:58 +0700

    fix form peminjaman

--- application/controllers/crudbarang.php
+++ application/controllers/crudbarang.php
@@ -4,7 +4,9 @@ class Crudbarang extends CI_Controller{
 
 	function __construct(){
 		parent::__construct();
-		$this->load->model('mo_barang');
+                $this->load->model('mo_barang');
+		if($this->session->userdata('status') != "login"){
+			redirect(base_url("login/masuk"));}
 	}
 	function kebarang(){
 		$data['tb_barang'] = $this->mo_barang->tampil_data()->result();
--- application/views/Vi_pinjambarang.php
+++ application/views/Vi_pinjambarang.php
@@ -97,29 +97,121 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </div>           
         </header><!-- /header -->
         <!-- Header-->
-<!-- TABEL-->
-        <div class="content mt-3">
-            <div class="animated fadeIn">
-                <div class="row">
-
-                <div class="col-md-12">
+<div class="container">
                     <div class="card">
-                        <div class="card-header">
-                            <strong class="card-title">Pinjam Barang</strong>
-                        </div>
-                        <div class="card-body">
-                  <table id="bootstrap-data-table" class="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                        <th>No SPT</th>
-                        <th>Tanggal</th>
-                        <th>Barang</th>
-                        </tr>       
-                    </thead>
-                    <tbody>
-                  
-                    </tbody>
-                </table>
+                      <div class="card-header">
+                        <strong>Form Peminjaman</strong>
+                      </div>
+                      <div class="card-body card-block">
+                        <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">Text Input</label></div>
+                            <div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="Text" class="form-control"><small class="form-text text-muted">This is a help text</small></div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="email-input" class=" form-control-label">Email Input</label></div>
+                            <div class="col-12 col-md-9"><input type="email" id="email-input" name="email-input" placeholder="Enter Email" class="form-control"><small class="help-block form-text">Please enter your email</small></div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="password-input" class=" form-control-label">Password</label></div>
+                            <div class="col-12 col-md-9"><input type="password" id="password-input" name="password-input" placeholder="Password" class="form-control"><small class="help-block form-text">Please enter a complex password</small></div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="disabled-input" class=" form-control-label">Disabled Input</label></div>
+                            <div class="col-12 col-md-9"><input type="text" id="disabled-input" name="disabled-input" placeholder="Disabled" disabled="" class="form-control"></div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="textarea-input" class=" form-control-label">Textarea</label></div>
+                            <div class="col-12 col-md-9"><textarea name="textarea-input" id="textarea-input" rows="9" placeholder="Content..." class="form-control"></textarea></div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="select" class=" form-control-label">Select</label></div>
+                            <div class="col-12 col-md-9">
+                              <select name="select" id="select" class="form-control">
+                                <option value="0">Please select</option>
+                                <option value="1">Option #1</option>
+                                <option value="2">Option #2</option>
+                                <option value="3">Option #3</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="selectLg" class=" form-control-label">Select Large</label></div>
+                            <div class="col-12 col-md-9">
+                              <select name="selectLg" id="selectLg" class="form-control-lg form-control">
+                                <option value="0">Please select</option>
+                                <option value="1">Option #1</option>
+                                <option value="2">Option #2</option>
+                                <option value="3">Option #3</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="selectSm" class=" form-control-label">Select Small</label></div>
+                            <div class="col-12 col-md-9">
+                              <select name="selectSm" id="SelectLm" class="form-control-sm form-control">
+                                <option value="0">Please select</option>
+                                <option value="1">Option #1</option>
+                                <option value="2">Option #2</option>
+                                <option value="3">Option #3</option>
+                                <option value="4">Option #4</option>
+                                <option value="5">Option #5</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="disabledSelect" class=" form-control-label">Disabled Select</label></div>
+                            <div class="col-12 col-md-9">
+                              <select name="disabledSelect" id="disabledSelect" disabled="" class="form-control">
+                                <option value="0">Please select</option>
+                                <option value="1">Option #1</option>
+                                <option value="2">Option #2</option>
+                                <option value="3">Option #3</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="row form-group">
+                            <div class="col col-md-3"><label for="multiple-select" class=" form-control-label">Multiple select</label></div>
+                            <div class="col col-md-9">
+                              <select name="multiple-select" id="multiple-select" multiple="" class="form-control">
+                                <option value="1">Option #1</option>
+                                <option value="2">Option #2</option>
+                                <option value="3">Option #3</option>
+                                <option value="4">Option #4</option>
+                                <option value="5">Option #5</option>
+                                <option value="6">Option #6</option>
+                                <option value="7">Option #7</option>
+                                <option value="8">Option #8</option>
+                                <option value="9">Option #9</option>
+                                <option value="10">Option #10</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="row form-group">
+                           
+                            <div class="col col-md-9">
+                            
+                            </div>
+                          </div>
+                          
+                          
+                        
+                        
+                        </form>
+                      </div>
+                      <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm">
+                          <i class="fa fa-dot-circle-o"></i> Submit
+                        </button>
+                        <button type="reset" class="btn btn-danger btn-sm">
+                          <i class="fa fa-ban"></i> Reset
+                        </button>
+                      </div>
+                    </div>
+                   
+                     
+                    </div>
+                  </div>
 
      
 
