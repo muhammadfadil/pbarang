ca6cddd7f31782117191543ff80dfa6ab30b70f3
isalanand <faisalanand@gmail.com>
2018-10-30 13:30:31 +0700

    Auth
    fix auth form peminjaman
    fix auth daftar barang

--- application/controllers/crudpinjambarang.php
+++ application/controllers/crudpinjambarang.php
@@ -5,6 +5,8 @@ class Crudpinjambarang extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->model('mo_pinjambarang');
+                if($this->session->userdata('status') != "login"){
+			redirect(base_url("login/masuk"));}
 	}
 	function kepinjambarang(){
 		$data['tbl_pinjambarang'] = $this->mo_pinjambarang->tampil_data()->result();
